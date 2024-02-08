ebc9834af3157a16f599a3077a7bbde9c01d7581
edd000 <63064292+edd000@users.noreply.github.com>
2024-02-07 20:42:00 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'App',
+  description: 'App',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Mi Página Next.js</h1>
+      <p>Bienvenido a mi página básica con Next.js</p>
+    </div>
+  );
+};
+
+export default HomePage;
