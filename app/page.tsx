1b62a6f3a59899b29c28e2c4c550cbece3b25fdc
edd000 <63064292+edd000@users.noreply.github.com>
2024-02-12 20:26:39 -0500

    1
    1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Mi Página Next.js</h1>
-      <p>Eres una bestia ,mojtro Next.js</p>
+    <div className="bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold mb-4">Bienvenido a mi sitio con Next.js y Tailwind UI</h1>
+      <p className="text-gray-600">Esta es la página de inicio.</p>
     </div>
   );
 };
