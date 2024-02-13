a2dc060a37f5577bc73cdfeb3a51d213c107755f
Maturome <159209607+Maturome@users.noreply.github.com>
2024-02-13 03:03:15 +0100

    2
    2

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className="bg-gray-100 p-8">
-      <h1 className="text-4xl font-bold mb-4">Bienvenido a mi sitio con Next.js y Tailwind UI</h1>
-      <p className="text-gray-600">Esta es la página de inicio.</p>
+    <div className="bg-gray-200 p-8">
+      <h1 className="text-4xl font-bold mb-4">Bienvenido</h1>
+      <p className="text-gray-600">Esta es el inicio de proyecto.</p>
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">
+        Botón de ejemplo  excelente
+      </button>
     </div>
   );
 };
 
 export default HomePage;
+
+
