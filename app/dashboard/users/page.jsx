0512bf501907deecea76f231872dafb856dd338b
edd000 <63064292+edd000@users.noreply.github.com>
2024-03-01 21:24:58 -0500

    3
    3

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,8 +1,65 @@
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
-const Homepage = () => {
+const UsersPage =  () => {
   return (
-    <div>usuarios</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>         
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src="/noavatar.png"
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                 jhon doe
+                </div>
+              </td>
+              <td>jhon@gmail.com</td>
+              <td>13.01</td>
+              <td>Admin</td>
+              <td>Active</td>
+              <td>
+                <div className={styles.button}>                
+                  <Link href="/dashboard/users/test">
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                    </Link>                              
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button> 
+                </div>                
+              </td>
+            </tr>         
+        </tbody>
+      </table> 
+      <Pagination/>     
+    </div>
+  );
+};
 
-export default Homepage
+export default UsersPage;
