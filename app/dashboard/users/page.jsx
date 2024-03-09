49d67e0ff3bbef982e03dc9e3e8b53a5b7bf1570
edd000 <63064292+edd000@users.noreply.github.com>
2024-03-08 20:47:53 -0500

    2:38
    2:38

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,14 +1,15 @@
+import { fetchUsers } from "@/app/lib/data";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
-import Link from "next/link";
 import Image from "next/image";
-import Pagination from "@/app/ui/dashboard/pagination/pagination";
-import { fetchUsers } from "@/app/lib/data";
+import Link from "next/link";
 
-const UsersPage = async () => {
+const UsersPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+  const { count, users } = await fetchUsers(q, page);
 
-  const users = await fetchUsers();
-  console. log(users)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -28,19 +29,19 @@ const UsersPage = async () => {
             <td>Action</td>
           </tr>
         </thead>
-        <tbody>  
-          {users.map(user=>(      
-            <tr  key={user.id}>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
               <td>
                 <div className={styles.user}>
                   <Image
-                    src={user.img ||"/noavatar.png"}
+                    src={user.img || "/noavatar.png"}
                     alt=""
                     width={40}
                     height={40}
                     className={styles.userImage}
                   />
-                 {user.username}
+                  {user.username}
                 </div>
               </td>
               <td>{user.email}</td>
@@ -48,22 +49,25 @@ const UsersPage = async () => {
               <td>{user.isAdmin ? "Admin" : "Client"}</td>
               <td>{user.isActive ? "active" : "passive"}</td>
               <td>
-                <div className={styles.button}>                
+                <div className={styles.buttons}>
                   <Link href={`/dashboard/users/${user.id}`}>
                     <button className={`${styles.button} ${styles.view}`}>
                       View
                     </button>
-                    </Link>                              
+                  </Link>
+                  <form>
+                    <input type="hidden" name="id" value={(user.id)} />
                     <button className={`${styles.button} ${styles.delete}`}>
                       Delete
-                    </button> 
-                </div>                
+                    </button>
+                  </form>
+                </div>
               </td>
-            </tr> 
-           ))}        
+            </tr>
+          ))}
         </tbody>
-      </table> 
-      <Pagination/>     
+      </table>
+      <Pagination count={count} />
     </div>
   );
 };
---
+++ app/lib/actions.js
@@ -0,0 +1,28 @@
+"use server";
+
+import {User} from "./models";
+import { connectToDB } from "./utils";
+
+export const addUser = async (formData) => {  
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    
+    const newUser = new User({
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create user!");
+  }  
+};
